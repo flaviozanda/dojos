5a85cb34883a25f98c79aa7a83b4cb540704273c
Adam Brown <adampsbrown@googlemail.com>
2014-11-24 11:12:50 +0000

    Update AnagramsTest.java

--- anagrams/20141012-adam-stefan/src/test/java/AnagramsTest.java
+++ anagrams/20141012-adam-stefan/src/test/java/AnagramsTest.java
@@ -42,4 +42,13 @@ public class AnagramsTest {
         assertThat(output.size()).isEqualTo(BIRO_MAX_COMBINATIONS);
     }    
     
+    @Test
+    public void testMaximumLetterCount3() {
+        Anagrams anagrams = new Anagrams("biro");
+
+        List<String> output = anagrams.generate();
+
+        assertThat(output.size()).isEqualTo(BIRO_MAX_COMBINATIONS);
+    }    
+    
 }
